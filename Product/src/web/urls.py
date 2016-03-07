8e9fd852ab7d9dea5cea4acee97fcff5a69ebf81
Mads Buchmann <buchmannf@mail.com>
2016-03-07 15:48:07 +0100

    add index page

--- Product/src/web/urls.py
+++ Product/src/web/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 #specify views
 from web import views
 urlpatterns = [
-    url(r'^projects$', views.project_list),
+    url(r'^$', views.index, name="index"),
+    url(r'^projects$', views.project_list, name="project_list"),
 ]
--- Product/src/web/views.py
+++ Product/src/web/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Project
 # Create your views here.
+
+def index(request):
+    return render(request,"web/index.html")
+
 def project_list(request):
     projects = Project.objects.all()
     context = {
