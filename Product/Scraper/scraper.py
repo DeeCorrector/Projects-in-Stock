f51635fbd6746082ecf45ab05dc3aba10b69bc49
Herluf <herlufbaggesen13@gmail.com>
2016-03-31 14:46:46 +0200

    add scraper request

--- Product/Scraper/scraper.py
+++ Product/Scraper/scraper.py
@@ -1,7 +1,13 @@
+import requests
+
 class IRequest():
   def get_html(self):
       pass
 
-class Request():
+class Request(IRequest):
     def __init__(self,_url):
         self.url = _url
+
+    def get_html(self):
+        response = requests.get(self.url)
+        return(response.text)
