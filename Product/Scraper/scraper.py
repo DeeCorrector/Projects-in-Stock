7288d21e68391ac2a2d40e53ffc497343449e642
Mads Buchmann <buchmannf@mail.com>
2016-03-31 13:07:06 +0200

    Begin development of scraper module

---
+++ Product/Scraper/scraper.py
@@ -0,0 +1,7 @@
+class IRequest():
+  def get_html(self):
+      pass
+
+class Request():
+    def __init__(self,_url):
+        self.url = _url
---
+++ Product/Scraper/tests.py
@@ -0,0 +1,35 @@
+from scraper import IRequest, Request
+import unittest as ut
+
+
+class Test_IRequest(ut.TestCase):
+    def setUp(self):
+        self.instance = IRequest()
+
+    def test_init(self):
+        self.assertTrue(type(self.instance), IRequest)
+
+    def test_contains_methods(self):
+        try:
+            self.instance.get_html()
+        except AttributeError:
+            self.fail("get_html does not exist!")
+
+
+class Test_Request(ut.TestCase):
+    def setUp(self):
+        self.instance = Request("www.test.testing/testing.html")
+
+    def test_contains_valid_fields(self):
+        try:
+            self.instance.url
+        except AttributeError:
+            self.fail('Field \'url\' does not exist')
+
+        self.assertFalse(self.instance.url=='')
+
+    def test_inherits_interface(self):
+        self.assertEquals(type(self.instance), IRequest)
+
+if __name__ == "__main__":
+    ut.main()
