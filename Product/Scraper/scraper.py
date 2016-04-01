63dbcbab526c52549ff2f5f29464e0a78d4b025d
Mads Buchmann <buchmannf@mail.com>
2016-04-01 14:05:06 +0200

    Add parse_html method functionality

--- Product/Scraper/scraper.py
+++ Product/Scraper/scraper.py
@@ -1,7 +1,10 @@
+#import the regular expressions
+import re
+#interface for request element
 class IRequest():
   def get_html(self):
       pass
-
+#concrete request class
 class Request(IRequest):
     def __init__(self,_url):
         self.url = _url
@@ -11,7 +14,6 @@ class Request(IRequest):
         return(response.text)
 
 #Interface for the parser
-"""Interface for the parser"""
 class IParser():
     def parse_html(self):
         pass
@@ -24,8 +26,25 @@ class Parser(IParser):
 
     def parse_html(self):
         #removes the html tags from a given string
-        def remove_tags(self,_string):
+        def remove_tags(_string):
             string_no_tags = re.sub("<.*?>","",_string)
             return string_no_tags
+        #Runs each regex and tries to find matches
+        for key in self.matching_dictonary:
+            matches_list = re.findall(self.matching_dictonary[key],self.html)
+            cleaned_matches_list = []
+            #removes tags from each result
+            for n in matches_list:
+                clean_string = remove_tags(n)
+                cleaned_matches_list.append(clean_string)
 
-        return {}
+            #Replace dictonary element with the new list
+            self.matching_dictonary[key] = cleaned_matches_list
+
+if __name__ == "__main__":
+    match_dict = {"link":"<a href.*?>.*?</a>",
+                  "header":"<h1 id class>.*?</h1>"}
+    parser = Parser("<h1 id class>Dette er en header</h1> <a href='google.com'>link</a><h1 id class>Anden<a href>link2</a> Header</h1>",match_dict)
+    parser.parse_html()
+    result = parser.matching_dictonary
+    print(result)
