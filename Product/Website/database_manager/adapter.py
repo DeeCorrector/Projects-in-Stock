36cad4e5d71fbe2d8126228d3f5fb6b508d91657
Mads Buchmann <buchmannf@mail.com>
2016-04-19 13:42:05 +0200

    make changes to adapter

--- Product/Website/database_manager/adapter.py
+++ Product/Website/database_manager/adapter.py
@@ -1,33 +1,10 @@
-from CommandManager.commandmanager import CommandManager, ICommand
+from ...CommandManager.commandmanager import CommandManager, ICommand
 from Scraper.scraper import Scraper
+import re
+import datetime
 
-class Adapter():
-    def __init__(self):
-        self.my_commandmanager = CommandManager()
-        self.my_commandFactory = CommandFactory()
-
-    #do db_update now
-    #schedule db_update at specific point in time
-    def schedule_update(self,datetime,counselor):
-        command = self.my_commandFactory.new_ScrapeCommand(datetime,couselor.url,counselor_match_dict,self.updatedb)
-        self.my_commandmanager.enqueue_command(command)
-
-    def updatedb(self,info_dict):
-        pass
-    #get all scheduled db_updates
-    def get_scheduled_updates(self):
-        return self.my_commandmanager.commandQueue
-
-    #Delete specific db_update
-    def delete_scheduled_update(self,cmd_id):
-        self.my_commandmanager.delete_command(cmd_id)
-    #clear all scheduled db_update
-    #Update database
-
-class CommandFactory():
-    def new_ScrapeCommand(self,_executionTime,_url,_match_dict,_target):
-        command = ScrapeCommand(_executionTime,_url,_match_dict,_target)
-        return command
+#for testing purposes
+counselor_match_dict = {"header": r"<h1 class='site-title'>.*?</h1>"}
 
 class ScrapeCommand (ICommand):
     def __init__(self,_executionTime, _url, _match_dict, _target ):
@@ -43,3 +20,40 @@ class ScrapeCommand (ICommand):
 
 class UpdateAllCounselors(ICommand):
     pass
+
+class CommandFactory():
+    def new_ScrapeCommand(self,_executionTime,_url,_match_dict,_target):
+        command = ScrapeCommand(_executionTime,_url,_match_dict,_target)
+        return command
+
+class Adapter():
+    def __init__(self):
+        self.my_commandmanager = CommandManager()
+        self.my_commandFactory = CommandFactory()
+
+    #do db_update now
+    #schedule db_update at specific point in time
+    def schedule_update(self,datetime,counselor):
+        command = self.my_commandFactory.new_ScrapeCommand(datetime,couselor.url,counselor_match_dict,self.updatedb)
+        self.my_commandmanager.enqueue_command(command)
+
+    def updatedb(self,info_dict):
+        print (info_dict)
+
+    #get all scheduled db_updates
+    def get_scheduled_updates(self):
+        return self.my_commandmanager.commandQueue
+
+    #Delete specific db_update
+    def delete_scheduled_update(self,cmd_id):
+        self.my_commandmanager.delete_command(cmd_id)
+    #clear all scheduled db_update
+    #Update database
+
+class MockCounselor():
+    def __init__(self,_url):
+        self.url = _url
+
+if __name__ == "__main__":
+    a = Adapter()
+    a.schedule_update(datetime.datetime.now() + datetime.timedelta(seconds=1),MockCounselor("http://www.test.org/"))
