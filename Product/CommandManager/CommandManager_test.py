d6043be59d6ef54a81fe2e57daf2bf5b8e42cbcf
Mads Buchmann <buchmannf@mail.com>
2016-05-25 14:06:23 +0200

    fix that CommandManager_test process never ends

--- Product/CommandManager/CommandManager_test.py
+++ Product/CommandManager/CommandManager_test.py
@@ -24,26 +24,27 @@ class Test_ICommand(ut.TestCase):
 class Test_CommandManager(ut.TestCase):
     def setUp(self):
         self.instance = CommandManager()
+        self.instance.myTimer.cancel()
 
     #__init__: inits
     def test_init(self):
         self.instance = CommandManager()
         self.assertTrue(type(self.instance),CommandManager)
-
+        self.instance.myTimer.cancel()
     #__init__: contains commandQueue
     def test_contains_commandQueue(self):
         self.instance = CommandManager()
         self.assertTrue(type(self.instance.commandQueue),list)
-
+        self.instance.myTimer.cancel()
     #__init__: commandQueue is empty on init
     def test_is_commandQueue_empty(self):
         self.instance = CommandManager()
-
+        self.instance.myTimer.cancel()
     #__init__: contains myTimer
     def test_contains_myTimer(self):
         self.instance = CommandManager()
         self.assertTrue(type(self.instance.myTimer),datetime)
-
+        self.instance.myTimer.cancel()
     #enqueue_command: commandQueue is +1 longer after enqueue_command
     def test_que_is_longer(self):
         self.instance = CommandManager()
@@ -53,15 +54,14 @@ class Test_CommandManager(ut.TestCase):
         endLength = len(self.instance.commandQueue)
 
         self.assertTrue((startLength != endLength), True)
-
-
+        self.instance.myTimer.cancel()
     #enqueue_command: commandQueue contains the queued command
     def test_que_contains_new_command(self):
         self.instance = CommandManager()
         command = ICommand(datetime(2017,1,5))
         self.instance.enqueue_command(command)
         self.assertTrue((command in self.instance.commandQueue), True)
-
+        self.instance.myTimer.cancel()
     #delete_command: commandQueue is -1 longer after delete_command
     def test_que_is_shorter(self):
         self.instance = CommandManager()
@@ -73,7 +73,7 @@ class Test_CommandManager(ut.TestCase):
         endLength = len(self.instance.commandQueue)
 
         self.assertFalse((startLength == endLength), False)
-
+        self.instance.myTimer.cancel()
     #delete_command: commandQueue does not contain the deleted command
     def test_que_excludes_command(self):
         self.instance = CommandManager()
@@ -85,7 +85,7 @@ class Test_CommandManager(ut.TestCase):
         endContains  = (command in self.instance.commandQueue)
 
         self.assertFalse(endContains,startContains)
-
+        self.instance.myTimer.cancel()
     #create_new_timer: sets a new timer
     def test_sets_new_timer(self):
         self.instance = CommandManager()
@@ -97,7 +97,7 @@ class Test_CommandManager(ut.TestCase):
         newTimer = copy.copy(self.instance.myTimer)
 
         self.assertFalse((oldTimer == newTimer),False)
-
+        self.instance.myTimer.cancel()
     #execute_command: Head is not the same after execute has been run
     def test_head_is_not_same(self):
         self.instance = CommandManager()
@@ -111,7 +111,7 @@ class Test_CommandManager(ut.TestCase):
         self.instance.execute_command()
         startHead = (self.instance.commandQueue[:])[0]
         self.assertTrue((oldHead != startHead),True)
-
+        self.instance.myTimer.cancel()
     #execute_command: myTimer is updated after execute has been run
     def test_myTimer_is_updated(self):
         self.instance = CommandManager()
@@ -126,7 +126,7 @@ class Test_CommandManager(ut.TestCase):
         endTimer = copy.copy(self.instance.myTimer)
 
         self.assertTrue((startTimer != endTimer),True)
-
+        self.instance.myTimer.cancel()
     #main_loop: if there is something in the que it should be executed
     def test_is_head_executed(self):
         self.instance = CommandManager()
@@ -138,9 +138,10 @@ class Test_CommandManager(ut.TestCase):
         try:
             self.instance.commandQueue[0]
             self.fail("head is not executed")
+            self.instance.myTimer.cancel()
         except IndexError:
             self.assertTrue(True,True)
-
+            self.instance.myTimer.cancel()
     #main_loop: if commandQueue.length == 0 nothing should happen
     def test_empty_queue_behaviour(self):
         self.instance = CommandManager()
@@ -148,6 +149,7 @@ class Test_CommandManager(ut.TestCase):
         self.instance.main_loop()
         endQue = copy.copy(self.instance.commandQueue)
         self.assertTrue((startQue == endQue), True)
+        self.instance.myTimer.cancel()
 
     #main_loop: if commandQueue.length > 1 the  timer should be updated
     def test_is_myTimer_updated(self):
@@ -163,7 +165,7 @@ class Test_CommandManager(ut.TestCase):
         endTimer = copy.copy(self.instance.myTimer)
 
         self.assertTrue((startTimer != endTimer), True)
-
+        self.instance.myTimer.cancel()
     #main_loop: if commandQueue.length == 1 the timer should not be updated
     def test_myTimer_is_not_updated(self):
         self.instance = CommandManager()
@@ -176,6 +178,6 @@ class Test_CommandManager(ut.TestCase):
         endTimer = self.instance.myTimer
 
         self.assertTrue((startTimer == endTimer), True)
-
+        self.instance.myTimer.cancel()
 if __name__ == "__main__":
     ut.main()
