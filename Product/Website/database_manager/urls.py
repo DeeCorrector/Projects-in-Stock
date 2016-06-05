68d858397c27592903894d450f0629fcfb56f3e9
Mads Buchmann <buchmannf@mail.com>
2016-06-05 16:37:13 +0200

    Continued work on update manager

--- Product/Website/database_manager/urls.py
+++ Product/Website/database_manager/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^database/$', views.update_database_view, name="update_db"),
     url(r'^database/all/$', views.update_all_counselors, name="update_db_all"),
     url(r'^database/specific/$', views.update_specific_counselor, name="update_db_specific"),
-    url(r'^database/findnewcounselors',views.find_new_counselors, name="find_new_counselors")
+    url(r'^database/findnewcounselors',views.find_new_counselors, name="find_new_counselors"),
+    url(r'^database/post', views.update_database_post, name="update_db_post" )
 ]
--- Product/Website/database_manager/views.py
+++ Product/Website/database_manager/views.py
@@ -6,14 +6,30 @@ import datetime
 adapter = Adapter()
 
 def update_database_view(request):
+    #check for authorization
     if not request.user.is_authenticated():
         return redirect('/admin/login/')
     else:
         counselors = Counselor.objects.all()
         context = {
-            "counselors":counselors
+            "counselors":counselors,
+            "counselorerror": False
         }
+
         return render(request, 'database_manager/db_update_page.html',context)
+
+def update_database_post(request):
+    #if information is being posted
+    if request.method == "POST":
+        #if a schedule was requested
+        if request.POST["Action"] == "schedule":
+            #convert queryDict to python Dict
+            #if no counselors was selected return to page
+            print request.POST
+            for c in request.POST["Counselor"].iteritems():
+                print c
+
+            return redirect("http://google.com")
 
 def update_all_counselors(request):
     adapter.update_all_now()
